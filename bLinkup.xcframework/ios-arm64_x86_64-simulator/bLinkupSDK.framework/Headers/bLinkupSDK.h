83944ce41d2fe25c4094ed2d0bbbc82e75d37d3b
Oleksandr Chernov <alexsxchernov@gmail.com>
2024-06-03 09:37:53 +0100

    2.1.5

---
+++ bLinkup.xcframework/ios-arm64_x86_64-simulator/bLinkupSDK.framework/Headers/bLinkupSDK.h
@@ -0,0 +1,18 @@
+//
+//  bLinkupSDK.h
+//  bLinkupSDK
+//
+//  Created by Oleksandr Chernov on 03.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bLinkupSDK.
+FOUNDATION_EXPORT double bLinkupSDKVersionNumber;
+
+//! Project version string for bLinkupSDK.
+FOUNDATION_EXPORT const unsigned char bLinkupSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bLinkupSDK/PublicHeader.h>
+
+
